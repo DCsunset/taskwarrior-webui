a3ab682575ec1783ee5d412ae3f8ae0f3a98dbe1
DCsunset <dcsunset@protonmail.com>
2020-07-06 22:32:59 -0700

    feat(frontend): add vuex

---
+++ frontend/store/index.ts
@@ -0,0 +1,36 @@
+import { ActionTree, MutationTree } from 'vuex';
+import { Task } from 'taskwarrior-lib';
+import { getAccessorType } from 'typed-vuex';
+
+export const state = () => ({
+	tasks: [] as Task[]
+});
+
+export type RootState = ReturnType<typeof state>;
+
+export const mutations: MutationTree<RootState> = {
+	setTasks(state, tasks: Task[]) {
+		state.tasks = tasks;
+	}
+};
+
+export const actions: ActionTree<RootState, RootState> = {
+	async fetchTasks(context) {
+		const tasks: Task[] = await this.$axios.$get('/api/tasks');
+		context.commit('setTasks', tasks);
+	},
+
+	async deleteTasks(context, tasks: Task[]) {
+		await this.$axios.$delete('/api/tasks', {
+			params: { tasks: tasks.map(task => task.uuid) }
+		});
+		const newTasks = context.state.tasks.filter(task => tasks.findIndex(t => t.uuid === task.uuid) === -1);
+		context.commit('setTasks', newTasks);
+	}
+};
+
+export const accessorType = getAccessorType({
+	state,
+	mutations,
+	actions
+});
