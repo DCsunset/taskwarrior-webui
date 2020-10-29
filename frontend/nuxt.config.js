066c92946f3d2394b917ca0be697b53a2175ee61
DCsunset <dcsunset@protonmail.com>
2020-10-29 08:40:57 +0800

    chore(deps): bump versions of deps

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-	mode: 'spa',
+	ssr: false,
 	/*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
