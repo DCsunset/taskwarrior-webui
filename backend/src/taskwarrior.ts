2dd183e9dd82f9418c3944267a31a4f156190a15
DCsunset <DCsunset@users.noreply.github.com>
2022-11-08 11:35:49 -0500

    fix(backend): remove env overwriting

--- backend/src/taskwarrior.ts
+++ backend/src/taskwarrior.ts
@@ -1,9 +1,7 @@
 import { TaskwarriorLib } from 'taskwarrior-lib';
-import * as path from 'path';
 
-const prod = process.env.NODE_ENV === 'production';
-const taskRc = process.env.TASKRC || (prod ? '/.taskrc' : path.join(__dirname, '../test/.taskrc'));
-const taskData = process.env.TASKDATA || (prod ? '/.task' : path.join(__dirname, '../test/.task'));
+const taskRc = process.env.TASKRC;
+const taskData = process.env.TASKDATA;
 
 const taskwarrior = new TaskwarriorLib(taskRc, taskData);
 
