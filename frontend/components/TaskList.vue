41fabce38503138dbe4944338b950f2ed6f09b93
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-02-09 17:46:07 +0100

    Sorting urgent by most urgent first

--- frontend/components/TaskList.vue
+++ frontend/components/TaskList.vue
@@ -289,7 +289,7 @@ export default defineComponent({
 				: [{ text: 'Wait', value: 'wait' }]),
 			{ text: 'Until', value: 'until' },
 			{ text: 'Tags', value: 'tags' },
-			{ text: 'Urgency', value: 'urgency' },
+			{ text: 'Urgency', value: 'urgency', sort: (a: number, b: number) => !(a > b) },
 			{ text: 'Actions', value: 'actions', sortable: false }
 		]);
 
